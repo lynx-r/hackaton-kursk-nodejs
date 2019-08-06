433d1a0717e5fe81b0ef0fdbd7e4c79a63e6a543
lynx-r <aleksey.p10n@gmail.com>
2019-08-06 14:13:57 +0300

    fix callback

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ io.on('connection', function (socket) {
     socket.on('message', function (msg, fn) {
         console.log('broadcast message', msg);
         socket.broadcast.emit('message', msg);
-        fn({success: true, sent: msg});
+        if (typeof fn !== 'undefined') {
+            fn({success: true, sent: msg});
+        }
     });
     socket.on('disconnect', function () {
         console.log('a user disconnected');
